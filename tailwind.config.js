8092b85fe738d5e5605f8cc6fd0b9491172e1dbb
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-04-03 10:16:47 -0500

    tailwind and postcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{html,js}",
-        "*.html"
-    ],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}", "*.html"],
+  theme: {
+    colors: {
+      primary: "#C93B76",
+      secundary: "#212936",
+      "text-primary": "#E5E7EB",
+      "text-secundary": "#4D5562",
+      "text-secundary-2": "#121826a6",
     },
-    plugins: [],
-  }
+    fontFamily: {
+      inter: ['"Inter"', "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        'hero-bg': 'url("/src/assets/bg.jpg")'
+      }
+    },
+  },
+  plugins: [],
+};
